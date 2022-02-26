ac4e8a5c8170d9ff262a3aa712621f9e0ec863af
first_name <ss329222@gmail.com>
2022-02-26 12:05:45 +0530

    Added:MaximumOfThreeStrings

--- src/blz/demo/generics/main.java
+++ src/blz/demo/generics/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to check max of three variables");
-		Float x = 2.2f, y = 4.4f, z = 7.45f;
+		String x = "january", y = "February", z = "march";
 		TestMaximum.testMaximum(x, y, z);
 	}
 }
