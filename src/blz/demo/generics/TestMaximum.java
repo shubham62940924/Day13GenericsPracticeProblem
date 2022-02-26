1ec9aa4a63e025ae4db5fc1e03464f5caff6358e
first_name <ss329222@gmail.com>
2022-02-26 12:20:15 +0530

    Added:ExtendedMaxMethod

--- src/blz/demo/generics/TestMaximum.java
+++ src/blz/demo/generics/TestMaximum.java
@@ -1,35 +1,38 @@
 package blz.demo.generics;
-
-/* @purpose :// checking max of three Strings
+/* @purpose :// Extended method to make more than three parameter
   *we use compare The compareTo method returns a value that is negative, zero, 
   *or positive depending upon whether the argument is less than, equal to, or
   *greater than the given object
 */
 
-public class TestMaximum {
-	String x, y, z;
+public class TestMaximum<T extends Comparable<T>> {
+	private T x, y, z;
 
-	// constructor
-	public TestMaximum(String x, String y, String z) {
+//constructor
+	public TestMaximum(T x, T y, T z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
-	// checking max of three Strings
-	public static String testMaximum(String x, String y, String z) {
-		String max = x;// Initializing x as greater
-		if (y.compareTo(max) > 0)
-			max = y;// as of now y is greater
-
-		if (z.compareTo(max) > 0)
-			max = z;// now z is greater
-		printMax(x, y, z, max);
+//checking max of three Generic data
+	public static <T extends Comparable<T>> T testMaximum(T... values) {
+		T max = values[0];
+		for (int i = 1; i < values.length; i++) {
+			if (values[i].compareTo(max) > 0) {
+				max = values[i];
+			}
+		}
+		System.out.println("the maximum of");
+		for (int i = 0; i < values.length; i++) {
+			System.out.print(values[i] + "\t");
+		}
+		printMax(max);
 		return max;
-
 	}
 
-	public static void printMax(String x, String y, String z, String max) {
-		System.out.printf("the max of %s , %s and %s is %s\n", x, y, z, max);
+	public static <T> void printMax(T max) {
+		System.out.println("is\t" + max);
 	}
+
 }
