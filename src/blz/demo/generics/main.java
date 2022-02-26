ab04bb84cb7e9b07a762299aca0cc6c0294c3b05
first_name <ss329222@gmail.com>
2022-02-26 11:41:14 +0530

    Added:MaximimofThreeIntegers

--- src/blz/demo/generics/main.java
+++ src/blz/demo/generics/main.java
@@ -3,7 +3,7 @@ package blz.demo.generics;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to check max of three variables");
+		System.out.println("Welcome to  check max of three variables");
 		Integer x = 10, y = 30, z = 25;
 		TestMaximum.testMaximum(x, y, z);
 	}
