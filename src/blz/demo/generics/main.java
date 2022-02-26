11079b9e961bc5938cd325553cd54f67135eb4e2
first_name <ss329222@gmail.com>
2022-02-26 12:21:03 +0530

    Added:ExtendedMaxMethod

--- src/blz/demo/generics/main.java
+++ src/blz/demo/generics/main.java
@@ -4,7 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to check max of three variables");
-		String x = "january", y = "February", z = "march";
-		TestMaximum.testMaximum(x, y, z);
+		String x = "kai", y = "po", z = "che", m = "bro";
+		Integer a = 30, b = 39, c = 24;
+		Float p = 1.9f, q = 2.8f, r = 0.2f;
+		TestMaximum.<String>testMaximum(x, y, z, m);
+		TestMaximum.<Integer>testMaximum(a, b, c);
+		TestMaximum.<Float>testMaximum(p, q, r);
 	}
 }
