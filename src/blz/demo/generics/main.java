e2250a53ccfb282887236156f5d8e1e99510ffd5
first_name <ss329222@gmail.com>
2022-02-26 12:36:41 +0530

    Refactored:UsesGenericToCreateClass

--- src/blz/demo/generics/main.java
+++ src/blz/demo/generics/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to check max of three variables");
-		String x = "kai", y = "po", z = "che", m = "bro";
+		String x = "kai", y = "po", z = "che";
 		Integer a = 30, b = 39, c = 24;
 		Float p = 1.9f, q = 2.8f, r = 0.2f;
 		TestMaximum.testMaximum(x, y, z);
