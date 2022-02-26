bb1dd0b8d8d38498a83a0869d6d86014d864e5d6
first_name <ss329222@gmail.com>
2022-02-26 11:48:31 +0530

    Added:MaximumOfFloatNumbers

--- src/blz/demo/generics/TestMaximum.java
+++ src/blz/demo/generics/TestMaximum.java
@@ -1,6 +1,6 @@
 package blz.demo.generics;
 
-/* @purpose :// checking max of three integers
+/* @purpose :// checking max of three Floats
   *we use compare The compareTo method returns a value that is negative, zero, 
   *or positive depending upon whether the argument is less than, equal to, or
   *greater than the given object
@@ -15,20 +15,20 @@ public class TestMaximum {
 		this.z = z;
 	}
 
-	// checking maximum of three integers
-	public static Integer testMaximum(Integer x, Integer y, Integer z) {
-		Integer max = x;// Initializing x as greater
+	// checking max of three integers
+	public static Float testMaximum(Float x, Float y, Float z) {
+		Float max = x;// initialising x as greater
 		if (y.compareTo(max) > 0)
-			max = y;// y is greater
+			max = y;// as of now y is greater
 
 		if (z.compareTo(max) > 0)
-			max = z;// z is greater
+			max = z;// now z is greater
 		printMax(x, y, z, max);
 		return max;
 
 	}
 
-	public static void printMax(Integer x, Integer y, Integer z, Integer max) {
+	public static void printMax(Float x, Float y, Float z, Float max) {
 		System.out.printf("the max of %s , %s and %s is %s\n", x, y, z, max);
 	}
 }
